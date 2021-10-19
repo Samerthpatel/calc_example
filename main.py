cb52f43cf9f2956a0af43ad306a179360d45374e
samerthpatel <srp2266@njit.edu>
2021-10-19 13:18:25 -0400

    changes

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 """content of calculator.py"""
+
+
 def inc(x_value):
     """increment function adds one to the x_value"""
+
     return x_value + 1
+
+
 def test_answer():
     """this tests the function"""
+
     assert inc(4) == 5
